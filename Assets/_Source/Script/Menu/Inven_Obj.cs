4d00861b38c2fe354873cd054f94b5b70cb94308
JinYeolYune <66723679+engel0918@users.noreply.github.com>
2025-09-25 17:22:51 +0900

    inven

--- Assets/_Source/Script/Menu/Inven_Obj.cs
+++ Assets/_Source/Script/Menu/Inven_Obj.cs
@@ -1,7 +1,19 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Inven_Obj : MonoBehaviour
 {
     public GameObject Obj;
 
+    public List<string> EquipPT;
+
+    public List<string> RWP;
+    public List<string> MWP;
+
+    public List<string> GRN;
+    public List<int> GRN_Cnt;
+
+    public List<string> Con;
+    public List<int> Con_Cnt;
 }
