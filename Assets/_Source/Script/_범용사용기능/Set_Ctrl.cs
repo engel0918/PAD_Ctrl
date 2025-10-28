c5ee38dd2da2a9d75cbe5659de6ee2a734b7b22a
JinYeolYune <66723679+engel0918@users.noreply.github.com>
2025-10-28 18:02:11 +0900

    ing_01

--- Assets/_Source/Script/_범용사용기능/Set_Ctrl.cs
+++ Assets/_Source/Script/_범용사용기능/Set_Ctrl.cs
@@ -1,25 +1,55 @@
 using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class Set_Ctrl : MonoBehaviour
 {
+    [Header("Order: Gp, Res, Aud")]
+    [SerializeField] List<GameObject> Pages;
+    [SerializeField] List<Button> PageBtns;
+
     [Header("Order: GQ, AA, BM, RES")]
     [SerializeField] List<Dropdown> Dd_List;
 
     [Header("Order: Mv, BGMv, SFXv")]
     [SerializeField] List<Dropdown> Sld_List;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        
+        SetBtn();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        EventSystem.current.SetSelectedGameObject(PageBtns[0].gameObject);
+        func_PageCtrl(0);
+    }
+
+    void SetBtn()
+    {
+        if (PageBtns.Count > 0)
+        {
+            for (int i = 0; i <= (PageBtns.Count - 1); i++)
+            {
+                if (PageBtns[i] != null)
+                {
+                    int index = i;
+                    PageBtns[i].onClick.AddListener(() => func_PageCtrl(index));
+                }
+            }
+        }
+    }
+
+    public void func_PageCtrl(int i)
+    {
+        foreach (GameObject page in Pages)
+        {
+            if (page != null)
+            { page.SetActive(false); }
+        }
+
+        Pages[i].SetActive(true);
     }
 }
